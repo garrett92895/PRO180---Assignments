d27391144e8c22053de69eae8b845d8079ae73bd
Garrett Holbrook <garrett92895@yahoo.com>
2014-05-13 22:10:04 -0600

    v1.4
    Implemented a "HashPiece" class to map the piece symbols to their
    meanings (i.e. "Q" to "Queen")

--- src/file_io/HashPiece.java
+++ src/file_io/HashPiece.java
@@ -1,5 +1,21 @@
 package file_io;
 
 public class HashPiece {
-
+	private char[] pieceHashes = new char[] {'k', 'q', 'b', 'n', 'r', 'p', 'l', 'd'};
+	private String[] pieces = new String[] {"King", "Queen", "Bishop", "Knight", "Rook", "Pawn", "light", "dark"};
+	
+	public String returnPiece(char c)
+	{
+		int index = 0;
+		for(int i = 0; i < pieceHashes.length; i++)
+		{
+			if(pieceHashes[i] == c)
+			{
+				index = i;
+			}
+		}
+		
+		return pieces[index];
+	}
 }
+
--- src/file_io/MoveReader.java
+++ src/file_io/MoveReader.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class MoveReader {
 	private BufferedReader br;
+	HashPiece h;
 	private final String PIECES = "(q|k|b|n|r|p)";
 	private final String COLORS = "(l|d)";
 	private final String RANKS = "([a-h])";
@@ -16,7 +17,7 @@ public class MoveReader {
 	public MoveReader(String fileName)
 	{
 		File file = new File(fileName);
-		
+		h = new HashPiece();
 		try
 		{
 			br = new BufferedReader(new FileReader(fileName));
@@ -30,37 +31,45 @@ public class MoveReader {
 	
 	public void run()
 	{
-		String[] line;
-		
+		String line;
+		String[] directives;
 		try 
 		{
-			while((line = br.readLine().toLowerCase().split("\\s")) != null)
+			while((line = br.readLine()) != null)
 			{
+				directives = line.toLowerCase().split("\\s");
 				boolean matches = false;
 				
 				//Loops through the array of strings and checks to see if they match the requirements, if they don't then the
 				//for loop is finished
-				for(int i = 0; i < line.length; i++)
+				for(int i = 0; i < directives.length; i++)
 				{
-					if(line[i].matches((RANKS + COLUMNS) + "|" + ( PIECES + COLORS + RANKS + COLUMNS)))
+					if(directives[i].matches("(" + (RANKS + COLUMNS) + "|" + ( PIECES + COLORS + RANKS + COLUMNS) + ")" + "\\*?"))
 					{
 						matches = true;
 					}
 					else
 					{
 						matches = false;
-						i = line.length;
+						i = directives.length;
 					}
 				}
 				
 				if(matches)
 				{
-					parseMove(line);
+					parseMove(directives);
 				}
 			}
 		} 
 		catch (IOException e) 
 		{
+			e.printStackTrace();
+		}
+		
+		try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -70,9 +79,41 @@ public class MoveReader {
 		switch(chessMoves.length)
 		{
 		case 1:
-			
+			String s1 = "Places a " + h.returnPiece(chessMoves[0].charAt(0))
+					+ " of color " + h.returnPiece(chessMoves[0].charAt(1))
+					+ " at column " + chessMoves[0].charAt(2) 
+					+ " and row " + chessMoves[0].charAt(3);
+			System.out.println(s1);
+			break;
+		case 2:
+			String s2 = "";
+			if(!chessMoves[1].endsWith("*"))
+			{
+				s2 = "Takes the piece at column " + chessMoves[0].charAt(0)
+				+ " row " + chessMoves[0].charAt(1) 
+				+ " and places it at column" + chessMoves[1].charAt(0)
+				+ " row " + chessMoves[1].charAt(0);
+			}
+			else
+			{
+				s2 = "Takes the piece at column " + chessMoves[0].charAt(0)
+				+ " row " + chessMoves[0].charAt(1) 
+				+ " and captures the piece at column" + chessMoves[1].charAt(0)
+				+ " row " + chessMoves[1].charAt(0);
+			}
+			System.out.println(s2);
+			break;
+		case 4:
+			String s3 = "Moves the King from column " + chessMoves[0].charAt(0)
+			+ " row " + chessMoves[0].charAt(1)
+			+ " to column " + chessMoves[1].charAt(0)
+			+ " row " + chessMoves[1].charAt(1)
+			+ " and the Rook from column " + chessMoves[2].charAt(0)
+			+ " row " + chessMoves[2].charAt(1)
+			+ " to column " + chessMoves[3].charAt(0)
+			+ " row " + chessMoves[3].charAt(1);
+			System.out.println(s3);
 		}
 	}
-	
-	private String parseAbbr(char )
+
 }
